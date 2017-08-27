73311bf9f4785c30e2ae114f96cf96b8d4260acd
ashish madaan <ashishmadaan6@gmail.com>
2017-08-28 02:27:24 +0530

    Add regression model to our training set in R

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression1.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 4 - Simple Linear Regression/simple_linear_regression1.R
@@ -1,7 +1,7 @@
 # Data Preprocessing Template
 
 # import the dataset in dataset object
-dataset=read.csv('Data.csv')
+dataset=read.csv('Salary_Data.csv')
 #dataset= dataset[, 2:3]
 
 "installing a new library caTools to split our datasets into 
@@ -10,7 +10,7 @@ training and testing sets ans include it by using library command
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split=sample.split(dataset$Purchased,SplitRatio = 0.8)
+split=sample.split(dataset$Salary,SplitRatio = 2/3)
 training_set=subset(dataset,split==TRUE)
 test_set=subset(dataset,split==FALSE)
 
@@ -25,3 +25,17 @@ we have to include only those columns for scaling who have numeric values in dat
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
+" Fitting simple linear regression to the training set 
+    train the regressor on training set
+"
+#formula shows salary is related to years of experience
+# data shows the data on which we want to perform regression 
+regressor= lm(formula= Salary ~ YearsExperience,
+              data=training_set)
+"
+we can check details of our regression by typing summary(regression on console)
+it shows the number of stars higher the number more is the dependency of 
+dependent column on independent set and lesser the p value more is independent
+variable statistically significant
+"
