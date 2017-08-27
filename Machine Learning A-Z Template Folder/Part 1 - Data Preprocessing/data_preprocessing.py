9c670e8f9a46feddac20a7a27731a467d773a8a0
ashish madaan <ashishmadaan6@gmail.com>
2017-08-28 00:29:23 +0530

    modify data_preprocessing files

--- Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing.py
+++ Machine Learning A-Z Template Folder/Part 1 - Data Preprocessing/data_preprocessing.py
@@ -59,5 +59,8 @@ transform test set because its already fitted to training set
 '''
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)
+y_test = sc_y.transform(y_test)
  
 
