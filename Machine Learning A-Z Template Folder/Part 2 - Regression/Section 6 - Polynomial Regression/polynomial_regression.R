3ffb278074b854c2cd9afd3d5415447c029c5133
ashish madaan <ashishmadaan6@gmail.com>
2017-08-29 00:49:33 +0530

    Added linear and polynomial regression to poly_regg.R

--- Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
+++ Machine Learning A-Z Template Folder/Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.R
@@ -1,78 +1,61 @@
-# Polynomial Regression
+# Data Preprocessing Template
 
-# Importing the dataset
-dataset = read.csv('Position_Salaries.csv')
-dataset = dataset[2:3]
+# import the dataset in dataset object
+dataset=read.csv('Position_Salaries.csv')
+dataset= dataset[2:3]
 
-# Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
+"installing a new library caTools to split our datasets into 
+training and testing sets ans include it by using library command
+uncomment to split the dataset into test and training set
+"
+"
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split=sample.split(dataset$Purchased,SplitRatio = 0.8)
+training_set=subset(dataset,split==TRUE)
+test_set=subset(dataset,split==FALSE)
+"
 
+"
+machine learning models work on eucledian distances and we need to scale it as 
+it helps the algorithm to converge must faster
+Now we are going to learn how to handle scaling in machine learning that 
+helps us to check whether large data entries do not overshadow 
+small valued entries
+we have to include only those columns for scaling who have numeric values in dataset
+"
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
 
-# Fitting Linear Regression to the dataset
-lin_reg = lm(formula = Salary ~ .,
+"
+We are going to make both linear as well as polynomial regression and compare 
+both of them
+"
+
+#Fitting linear regression model to the dataset
+lin_reg = lm(Salary ~ . ,
              data = dataset)
 
-# Fitting Polynomial Regression to the dataset
-dataset$Level2 = dataset$Level^2
-dataset$Level3 = dataset$Level^3
-dataset$Level4 = dataset$Level^4
-poly_reg = lm(formula = Salary ~ .,
+"
+we will modify our matrix will add new independent variables that will be just the 
+powers of the first independent variables poly_reg will be used to perform that task
+"
+
+#Fitting polynomial regression model to the dataset
+dataset$level2= dataset$Level^2    # create a new level that is square of level 1
+dataset$level3= dataset$Level^3
+dataset$level4= dataset$Level^4
+poly_reg = lm(Salary ~ . ,
               data = dataset)
+summary(poly_reg)
 
-# Visualising the Linear Regression results
-# install.packages('ggplot2')
+#Visualizing the linear Regression results
+"
+we will plot a graph that will show the observation points and we will also plot
+linear regresion line
+we will plot a scatter graph that shows salaries in red colour and regression line in 
+blue colour.
+"
 library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Linear Regression)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Visualising the Polynomial Regression results
-# install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Polynomial Regression)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(poly_reg,
-                                        newdata = data.frame(Level = x_grid,
-                                                             Level2 = x_grid^2,
-                                                             Level3 = x_grid^3,
-                                                             Level4 = x_grid^4))),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Polynomial Regression)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Predicting a new result with Linear Regression
-predict(lin_reg, data.frame(Level = 6.5))
-
-# Predicting a new result with Polynomial Regression
-predict(poly_reg, data.frame(Level = 6.5,
-                             Level2 = 6.5^2,
-                             Level3 = 6.5^3,
-                             Level4 = 6.5^4))
